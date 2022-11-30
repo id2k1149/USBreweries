b8c4660f15a0750728ed052dd9ac6691fdf20f82
id2k1149 <id2k1149@gmail.com>
2022-11-30 09:44:38 -0500

    Initial Commit

---
+++ USBreweries/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  USBreweries
+//
+//  Created by Max Franz Immelmann on 11/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
