bf461f3a7c8d5272c27978e8cfafad77231b3c9b
id2k1149 <id2k1149@gmail.com>
2023-02-23 10:12:45 -0500

    add url link

--- USBreweries/ViewControllers/BreweryCell.swift
+++ USBreweries/ViewControllers/BreweryCell.swift
@@ -17,14 +17,31 @@ class BreweryCell: UITableViewCell {
     func configure(with brewery: Brewery) {
         breweryNameLabel.text = brewery.name
         streetLabel.text = "\(brewery.street ?? "N/A"), \(brewery.postal_code ?? "")"
-        webPageLabel.textColor = UIColor.blue
-        webPageLabel.text = "\(brewery.website_url ?? "N/A")"
+        
+        let linkString = "\(brewery.website_url ?? "N/A")"
+        let attributedString = NSMutableAttributedString(string: linkString)
+        let range = NSRange(location: 0, length: attributedString.length)
+        let linkAttributes: [NSAttributedString.Key: Any] = [
+            .link: URL(string: linkString) as Any,
+            .foregroundColor: UIColor.blue,
+            .underlineStyle: NSUnderlineStyle.single.rawValue
+        ]
+        attributedString.addAttributes(linkAttributes, range: range)
+        webPageLabel.attributedText = attributedString
+        webPageLabel.isUserInteractionEnabled = true
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(labelTapped(_:)))
+        webPageLabel.addGestureRecognizer(tapGesture)
+
         phoneLabel.font = UIFont.boldSystemFont(ofSize: 14)
         phoneLabel.text = "phone: \(brewery.phone ?? "N/A")"
         
-//        if let labelText = myLabel.text, let url = URL(string: labelText) {
-//            // Use the URL object here
-//        }
-
     }
+    
+    @objc func labelTapped(_ sender: UITapGestureRecognizer) {
+        if let attributedText = (sender.view as? UILabel)?.attributedText,
+            let url = attributedText.attribute(.link, at: 0, effectiveRange: nil) as? URL {
+                UIApplication.shared.open(url)
+        }
+    }
+
 }
